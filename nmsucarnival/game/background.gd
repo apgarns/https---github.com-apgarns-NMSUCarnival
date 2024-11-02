aa5df6b2d34a4b5670c635f99333a5943c1776ad
Fayaz shaik <75134586+shaik-fayaz@users.noreply.github.com>
2024-11-02 11:44:16 -0600

    updated

--- nmsucarnival/game/background.gd
+++ nmsucarnival/game/background.gd
@@ -4,18 +4,15 @@ extends Node2D
 
 var score
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_mobtimer_timeout() -> void:
 	var ene = enemy.instantiate()
 	ene.position = position
-	get.parent().addchild(ene)
+	get_parent().add_child(ene)
